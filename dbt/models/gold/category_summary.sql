d5a87a488ccaa45b0c977db5f93aa03cd66297d6
Tim Buchhorn <tim.buchhorn@snowflake.com>
2026-06-18 09:14:33 +1000

    chore: add dbt/models/gold/category_summary.sql

---
+++ dbt/models/gold/category_summary.sql
@@ -0,0 +1,9 @@
+SELECT
+    oi.PRODUCT_NAME,
+    COUNT(DISTINCT oi.ITEM_ID)          AS items_sold,
+    SUM(oi.QUANTITY)                    AS total_units,
+    SUM(oi.QUANTITY * oi.UNIT_PRICE)    AS total_revenue,
+    AVG(oi.UNIT_PRICE)                  AS avg_unit_price,
+    COUNT(DISTINCT oi.ORDER_ID)         AS order_count
+FROM {{ ref('order_items') }} oi
+GROUP BY 1
