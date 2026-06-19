bcdd77a086fb6a25f24420ebe1149ed162c48a5e
Tim Buchhorn <tim.buchhorn@snowflake.com>
2026-06-19 17:42:36 +1000

    schema(new_column): update dbt/models/gold/orders_daily.sql

--- dbt/models/gold/orders_daily.sql
+++ dbt/models/gold/orders_daily.sql
@@ -14,6 +14,7 @@ SELECT
     c.LOYALTY_TIER,
     c.PREFERRED_CONTACT,
     c.PREFERRED_LANGUAGE,
+    o.DISCOUNT_CODE,
     COUNT(DISTINCT o.ORDER_ID)         AS order_count,
     COUNT(DISTINCT o.CUSTOMER_ID)      AS customer_count,
     SUM(o.TOTAL_AMOUNT)                AS total_revenue,
@@ -21,4 +22,4 @@ SELECT
 FROM {{ ref('orders') }}    o
 JOIN {{ ref('customers') }} c
   ON o.CUSTOMER_ID = c.CUSTOMER_ID
-GROUP BY 1,2,3,4,5,6,7,8
+GROUP BY 1,2,3,4,5,6,7,8,9
